b41bfccb799be6e291d21f184f9d518a3f97d433
[Arturo] <arturocervantez27@gmail.com>
2022-11-05 22:16:48 -0600

    some env Changed

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
     e.preventDefault();
 
     var creds = new AWS.Credentials({
-      accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+      accessKeyId: AWS_ACCESS_KEY_ID,
+      secretAccessKey: AWS_SECRET_ACCESS_KEY,
     });
 
     var myConfig = new AWS.Config({
