3287d1eb45fc719b0cb0c0d216f116409a186148
Arturo Cervantes <101757303+ArturoHost@users.noreply.github.com>
2022-11-05 22:35:23 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
     e.preventDefault();
 
     var creds = new AWS.Credentials({
-      accessKeyId: AKIA3YA5II75CBZJ4NXM,
-      secretAccessKey: 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,
+      accessKeyId: "AKIA3YA5II75CBZJ4NXM",
+      secretAccessKey: "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",
     });
 
     var myConfig = new AWS.Config({
