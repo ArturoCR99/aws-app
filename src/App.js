e04dc4040ab3b54348b6a4c40d68639f5b5306da
[Arturo] <arturocervantez27@gmail.com>
2022-11-06 14:29:19 -0600

    New Commit

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ function App() {
     var creds = new AWS.Credentials({
       accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-      accessKeyId: "AKIA3YA5II75CBZJ4NXM",
-      secretAccessKey: "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",
     });
 
     var myConfig = new AWS.Config({
